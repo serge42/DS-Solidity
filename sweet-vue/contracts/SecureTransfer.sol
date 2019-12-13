162f28d7abfbc51b142b3088a88378054a52411a
Sebastien Bouquet <bouquet.sebastien@gmail.com>
2019-12-13 17:12:03 +0100

    updated SecureTransfer contract

--- sweet-vue/contracts/SecureTransfer.sol
+++ sweet-vue/contracts/SecureTransfer.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.5.8;
 contract SecureTransfer {
 
     uint public price;
-    uint offer;
-    address seller;
-    address buyer;
-    bool sellerAccept;
+    uint public offer;
+    address public seller;
+    address public buyer;
+    bool public sellerAccept;
 
     constructor() public {
         price = 0;
@@ -16,43 +16,46 @@ contract SecureTransfer {
     }
 
     function setPrice(uint _price) public {
-        require(msg.sender == seller, "");
-        require(price == 0, "");
+        require(msg.sender == seller, "price can only be set by seller");
+        require(_price > 0, "price should be larger than 0");
+        require(!sellerAccept, "an offer has already been accepted");
         price = _price;
     }
 
     function makeDeposit(address _buyer) public payable {
         require(_buyer != address(0), "invalide buyer address");
-        require(msg.value >= price, "amount is too small");
+        require(price > 0 && msg.value >= price && msg.value > offer, "amount is too small");
+        require(!sellerAccept, "An offer has already been accepted");
         buyer = _buyer;
         offer = msg.value;
     }
 
     function acceptOffer() public {
-        require(msg.sender == seller, "");
-        require(offer >= price, "can't accept offer below initial price");
+        require(msg.sender == seller, "only seller can accept an offer");
+        require(price > 0 && offer >= price, "can't accept offer below initial price");
         sellerAccept = true;
     }
 
     function endTransactionBuyer(address payable beneficiary) public {
-        require(msg.sender == buyer, "");
-        require(sellerAccept, "");
+        require(msg.sender == buyer, "only buyer can use this");
+        require(seller == beneficiary, "beneficiary has to be == seller");
+        require(sellerAccept, "seller has to accept the offer first");
 
         beneficiary.transfer(offer);
     }
 
     function endTransaction(address payable beneficiary) public {
-        require(msg.sender == seller, "");
-        require(sellerAccept, "");
-        //require(enough time has passed);
+        require(msg.sender == seller, "only seller can use this");
+        require(sellerAccept, "seller has to accept the offer first");
+        //require(enough time has passed, "has to wait longer");
         beneficiary.transfer(offer);
     }
 
-    function checkDeposit() public view returns (uint) {
-        return offer;
-    }
+    // function checkDeposit() public view returns (uint) {
+    //     return offer;
+    // }
 
-    function checkBuyer() public view returns (address) {
-        return buyer;
-    }
+    // function checkBuyer() public view returns (address) {
+    //     return buyer;
+    // }
 }
