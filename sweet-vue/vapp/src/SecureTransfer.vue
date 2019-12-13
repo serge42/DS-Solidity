356261047c1666e438d7c283bfa082aa631aa08c
Sebastien Bouquet <bouquet.sebastien@gmail.com>
2019-12-13 20:56:16 +0100

    added a test if current account is seller in SecureTransfer component in dapp

--- sweet-vue/vapp/src/SecureTransfer.vue
+++ sweet-vue/vapp/src/SecureTransfer.vue
@@ -1,23 +1,51 @@
 <template>
-    <div v-if="isDrizzleInitialized">
-        <div>
-            <drizzle-contract 
+  <div v-if="isDrizzleInitialized">
+    <div>
+        <drizzle-contract 
+            contractName="SecureTransfer" 
+            method="seller" 
+            label="Seller" />
+        <drizzle-contract 
+            contractName="SecureTransfer" 
+            method="price" 
+            label="Current price" />
+        <div v-if="isSeller">
+            <drizzle-contract-form
                 contractName="SecureTransfer"
-                method="price"
-                label="Current price"
+                method="setPrice"
+                :placeholders="['Set price (Wei)']"
+            />
+        </div>
+        <div v-else>
+            <drizzle-contract-form
+                contractName="SecureTransfer"
+                method="makeDeposit"
+                :placeholders="['Place offer (Wei)']"
             />
         </div>
     </div>
+  </div>
 
-    <div v-else>Loading...</div>
+  <div v-else>Loading...</div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-    name: 'SecureTransfer',
-    computed: mapGetters('drizzle', ['isDrizzleInitialized'])
+  name: "SecureTransfer",
+  computed:{
+  ...mapGetters('drizzle', ['isDrizzleInitialized','drizzleInstance']),
+  ...mapGetters('accounts', ['activeAccount']),
+  ...mapGetters('contracts', ['getContractData']),
+  isSeller() {
+    const seller = this.getContractData({
+      contract: 'SecureTransfer',
+      method: 'seller'
+    });
+    return seller === this.activeAccount;
+  }
+  }
 }
 </script>
 
