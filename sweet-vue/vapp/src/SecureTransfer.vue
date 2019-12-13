4d8fe89f8a2805cdaa34ea789f83a255025b137d
Sebastien Bouquet <bouquet.sebastien@gmail.com>
2019-12-13 17:19:25 +0100

    new vue component for SecureTransfer

---
+++ sweet-vue/vapp/src/SecureTransfer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="isDrizzleInitialized">
+        <div>
+            <drizzle-contract 
+                contract-name="SecureTransfer"
+                method="price"
+                label="Current price"
+            />
+        </div>
+    </div>
+
+    <div v-else>Loading...</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'SecureTransfer',
+    computed: mapGetters('drizzle', ['isDrizzleInitialized'])
+}
+</script>
+
+<style>
+
+</style>
