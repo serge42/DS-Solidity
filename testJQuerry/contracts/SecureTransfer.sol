4a09b865d6497a24a5f45cc91599249e37592235
Sebastien Bouquet <bouquet.sebastien@gmail.com>
2019-12-17 14:27:52 +0100

    changed sellerAccept to isActive

--- testJQuerry/contracts/SecureTransfer.sol
+++ testJQuerry/contracts/SecureTransfer.sol
@@ -6,19 +6,19 @@ contract SecureTransfer {
     uint public offer;
     address payable public seller;
     address public buyer;
-    bool public sellerAccept;
+    bool public isActive;
 
     constructor() public {
         price = 0;
         offer = 0;
-        sellerAccept = false;
+        isActive = true;
         seller = msg.sender;
     }
 
     function setPrice(uint _price) public {
         require(msg.sender == seller, "price can only be set by seller");
         require(_price > 0, "price should be larger than 0");
-        require(!sellerAccept, "an offer has already been accepted");
+        require(isActive, "an offer has already been accepted");
         require(offer == 0, "price can't be changed when an offer has already been made");
         price = _price;
     }
@@ -26,7 +26,7 @@ contract SecureTransfer {
     function makeDeposit(uint amt) public payable {
         require(msg.sender != address(0), "invalide buyer address");
         require(price > 0 && amt >= price && amt > offer, "amount is too small");
-        require(!sellerAccept, "An offer has already been accepted");
+        require(isActive, "An offer has already been accepted");
         buyer = msg.sender;
         offer = amt;
     }
@@ -34,20 +34,20 @@ contract SecureTransfer {
     function acceptOffer() public {
         require(msg.sender == seller, "only seller can accept an offer");
         require(price > 0 && offer >= price, "can't accept offer below initial price");
-        sellerAccept = true;
+        isActive = false;
     }
 
     function endTransactionBuyer(/* address payable beneficiary */) public {
         require(msg.sender == buyer, "only buyer can use this");
         // require(seller == beneficiary, "beneficiary has to be == seller");
-        require(sellerAccept, "seller has to accept the offer first");
+        require(!isActive, "seller has to accept the offer first");
 
         seller.transfer(offer);
     }
 
     function endTransaction(address payable beneficiary) public {
         require(msg.sender == seller, "only seller can use this");
-        require(sellerAccept, "seller has to accept the offer first");
+        require(!isActive, "seller has to accept the offer first");
         //require(enough time has passed, "has to wait longer");
         beneficiary.transfer(offer);
     }
